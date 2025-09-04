43bfa5cfff60852f730c019146965917456fa210
Marcelo Campana <cam17010@byui.edu>
2025-09-03 23:14:27 -0400

    Y el formato resuelto.

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -10,9 +10,15 @@ const route = useRoute()
 const { toc } = useAppConfig()
 const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
 
-const { data: page } = await useAsyncData(route.path, () => queryCollection('docs').path(route.path).first())
+const { data: page } = await useAsyncData(route.path, () =>
+  queryCollection('docs').path(route.path).first()
+)
 if (!page.value) {
-  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page not found',
+    fatal: true
+  })
 }
 
 const { data: surround } = await useAsyncData(`${route.path}-surround`, () => {
@@ -67,53 +73,37 @@ const links = computed(() => {
         />
       </template>
       <template #default>
-            <UBadge
+        <UBadge
           v-for="(tag, index) in page.meta?.tags || []"
           :key="index"
           color="neutral"
           variant="outline"
           size="xs"
           class="capitalize mr-1 mt-3"
-        >{{ tag }}</UBadge>
+        >
+          {{ tag }}
+        </UBadge>
       </template>
     </UPageHeader>
 
     <UPageBody>
-      <ContentRenderer
-        v-if="page"
-        :value="page"
-      />
+      <ContentRenderer v-if="page" :value="page" />
 
       <USeparator v-if="surround?.length" />
 
       <UContentSurround :surround="surround" />
     </UPageBody>
 
-    <template
-      v-if="page?.body?.toc?.links?.length"
-      #right
-    >
-      <UContentToc
-        :title="toc?.title"
-        :links="page.body?.toc?.links"
-      >
-        <template
-          v-if="toc?.bottom"
-          #bottom
-        >
+    <template v-if="page?.body?.toc?.links?.length" #right>
+      <UContentToc :title="toc?.title" :links="page.body?.toc?.links">
+        <template v-if="toc?.bottom" #bottom>
           <div
             class="hidden lg:block space-y-6"
             :class="{ '!mt-6': page.body?.toc?.links?.length }"
           >
-            <USeparator
-              v-if="page.body?.toc?.links?.length"
-              type="dashed"
-            />
+            <USeparator v-if="page.body?.toc?.links?.length" type="dashed" />
 
-            <UPageLinks
-              :title="toc.bottom.title"
-              :links="links"
-            />
+            <UPageLinks :title="toc.bottom.title" :links="links" />
           </div>
         </template>
       </UContentToc>
