3b08226f2a0778f1ca7ace350115ca13ef05d65c
Marcelo Campana <cam17010@byui.edu>
2025-09-04 14:38:07 -0400

    Update site name and format links in app configuration; enhance content schema for links

--- app/app.config.ts
+++ app/app.config.ts
@@ -14,7 +14,7 @@ export default defineAppConfig({
     }
   },
   seo: {
-    siteName: 'Nuxt Docs Template'
+    siteName: 'Mis notas'
   },
   header: {
     title: '',
@@ -26,59 +26,70 @@ export default defineAppConfig({
     },
     search: true,
     colorMode: true,
-    links: [{
-      'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt-ui-pro/docs',
-      'target': '_blank',
-      'aria-label': 'GitHub'
-    }]
+    links: [
+      {
+        icon: 'i-simple-icons-github',
+        to: 'https://github.com/nuxt-ui-pro/docs',
+        target: '_blank',
+        'aria-label': 'GitHub'
+      }
+    ]
   },
   footer: {
     credits: `Copyright © ${new Date().getFullYear()}`,
     colorMode: false,
-    links: [{
-      'icon': 'i-simple-icons-nuxtdotjs',
-      'to': 'https://nuxt.com',
-      'target': '_blank',
-      'aria-label': 'Nuxt Website'
-    }, {
-      'icon': 'i-simple-icons-discord',
-      'to': 'https://discord.com/invite/ps2h6QT',
-      'target': '_blank',
-      'aria-label': 'Nuxt UI on Discord'
-    }, {
-      'icon': 'i-simple-icons-x',
-      'to': 'https://x.com/nuxt_js',
-      'target': '_blank',
-      'aria-label': 'Nuxt on X'
-    }, {
-      'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt/ui',
-      'target': '_blank',
-      'aria-label': 'Nuxt UI on GitHub'
-    }]
+    links: [
+      {
+        icon: 'i-simple-icons-nuxtdotjs',
+        to: 'https://nuxt.com',
+        target: '_blank',
+        'aria-label': 'Nuxt Website'
+      },
+      {
+        icon: 'i-simple-icons-discord',
+        to: 'https://discord.com/invite/ps2h6QT',
+        target: '_blank',
+        'aria-label': 'Nuxt UI on Discord'
+      },
+      {
+        icon: 'i-simple-icons-x',
+        to: 'https://x.com/nuxt_js',
+        target: '_blank',
+        'aria-label': 'Nuxt on X'
+      },
+      {
+        icon: 'i-simple-icons-github',
+        to: 'https://github.com/nuxt/ui',
+        target: '_blank',
+        'aria-label': 'Nuxt UI on GitHub'
+      }
+    ]
   },
   toc: {
     title: 'Table of Contents',
     bottom: {
       title: 'Community',
       edit: 'https://github.com/nuxt-ui-pro/docs/edit/main/content',
-      links: [{
-        icon: 'i-lucide-star',
-        label: 'Star on GitHub',
-        to: 'https://github.com/nuxt/ui',
-        target: '_blank'
-      }, {
-        icon: 'i-lucide-book-open',
-        label: 'Nuxt UI Pro docs',
-        to: 'https://ui.nuxt.com/getting-started/installation/pro/nuxt',
-        target: '_blank'
-      }, {
-        icon: 'i-simple-icons-nuxtdotjs',
-        label: 'Purchase a license',
-        to: 'https://ui.nuxt.com/pro/purchase',
-        target: '_blank'
-      }]
+      links: [
+        {
+          icon: 'i-lucide-star',
+          label: 'Star on GitHub',
+          to: 'https://github.com/nuxt/ui',
+          target: '_blank'
+        },
+        {
+          icon: 'i-lucide-book-open',
+          label: 'Nuxt UI Pro docs',
+          to: 'https://ui.nuxt.com/getting-started/installation/pro/nuxt',
+          target: '_blank'
+        },
+        {
+          icon: 'i-simple-icons-nuxtdotjs',
+          label: 'Purchase a license',
+          to: 'https://ui.nuxt.com/pro/purchase',
+          target: '_blank'
+        }
+      ]
     }
   }
 })
